0a8d017aac120ad2ee98f595dbe0779d4e2380b3
mohitpanchal09 <panchalmohitg2002@gmail.com>
2022-07-09 23:19:23 +0530

    initial commit

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -3,9 +3,13 @@ import styled from 'styled-components'
 import { mobile } from '../responsive'
 import { mobile2 } from '../responsive'
 import {mobile7} from'../responsive'
+import {mobile3} from '../responsive'
 const Container = styled.div`
 background-color:#1E1E1E;
 text-align:center;
+display:flex;
+align-items:center;
+justify-content:center;
 
 `
 const Wrapper = styled.div`
@@ -49,7 +53,7 @@ font-size:20px;
 color:#fffff;
 ${mobile({fontSize:"15px"})}
 ${mobile2({fontSize:"12px"})}
-
+${mobile3({fontSize:"10px"})}
 `
 const InfoContainer = styled.div`
 display:flex;
@@ -61,11 +65,14 @@ justify-content:center;
 `
 const Image = styled.img`
 height:60vh;
+
 ${mobile({height:"45vh"})}
-${mobile7({height:"35vh"})}
+${mobile7({height:"30vh"})}
+${mobile3({height:"22vh"})}
 `
 const Circle = styled.div`
 display:flex;
+
 justify-content:center;
 align-items:center;
 border-radius: 50%;
@@ -76,6 +83,7 @@ margin:20px -100px;
 // box-shadow: 0px 10.3879px 12.6138px rgba(0, 0, 0, 0.25);
 ${mobile({height:"170px",width:"170px",margin:"20px -90px"})}
 ${mobile2({height:"130px",width:"130px",margin:"20px -70px"})}
+${mobile3({height:"110px",width:"110px",margin:"20px -65px"})}
 `
 
 const Features = () => {
