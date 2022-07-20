6f8c86d7fd82bed44b90180f8817809019ee0c68
mohitpanchal09 <90472961+mohitpanchal09@users.noreply.github.com>
2022-07-20 12:58:21 +0530

    Create Services.jsx

---
+++ src/components/Services.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import styled from 'styled-components'
+import { popularProducts } from '../data';
+import Service from './Service';
+import { mobile8 } from '../responsive'
+import { mobile4 } from '../responsive'
+const Container=styled.div`
+    padding:0px;
+    display:flex;
+    flex-wrap: wrap;
+    align-items:center;
+    justify-content: center;
+    background-color:#1E1E1E;
+`
+;
+const Title = styled.p`
+color: #AFAFAF;
+font-weight: 700;
+font-size: 60.0775px;
+text-align:center;
+font-family:'syne';
+padding:20px;
+${mobile8({fontSize:"40px"})}
+${mobile4({fontSize:"30px"})}
+
+`
+const Wrapper = styled.div`
+background-color:#1E1E1E;
+padding:50px 0px;
+
+`
+
+function Services() {
+  return (
+    <Wrapper>
+        <Title>Our Services</Title>
+    <Container>
+     
+     {popularProducts.map((item) => (
+        <Service item ={item} key={item.id} />
+    ))}
+
+
+    </Container>
+    </Wrapper>
+  )
+}
+
+export default Services
