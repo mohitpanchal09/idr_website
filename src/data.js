71128dd69cd6866760cf97dbc0c2ca120e640779
mohitpanchal09 <90472961+mohitpanchal09@users.noreply.github.com>
2022-07-20 13:02:11 +0530

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,74 @@
+
+export const popularProducts = [
+    {
+      id:1,
+      img:"https://cdn.discordapp.com/attachments/992390118457483376/999034625643008040/unknown.png",
+      title:"Research & Development",
+    },
+    {
+      id:2,
+      img:"https://cdn.discordapp.com/attachments/992390118457483376/999035394895134740/unknown.png",
+      title:"Designing",
+    },
+    {
+      id:3,
+      img:"https://cdn.discordapp.com/attachments/992390118457483376/999035560935038976/unknown.png",
+      title:"Drones Uavs Training Program",
+    },
+    {
+      id:4,
+      img:"https://cdn.discordapp.com/attachments/992390118457483376/999035731639013506/unknown.png",
+      title:"Aerial Video Photography",
+    },
+    {
+      id:5,
+      img:"https://cdn.discordapp.com/attachments/992390118457483376/999035787171594330/unknown.png",
+      title:"Product Development",
+    },
+    {
+      id:6,
+      img:"https://cdn.discordapp.com/attachments/992390118457483376/999035818528227378/unknown.png",
+      title:"3D Printing"
+    },
+    
+
+  ]
+
+
+
+
+
+  
+export const popularIndustry = [
+  {
+    id:1,
+    img:"https://cdn.discordapp.com/attachments/992390118457483376/999193406402215936/unknown.png",
+    title:"Defence Industry",
+  },
+  {
+    id:2,
+    img:"https://cdn.discordapp.com/attachments/992390118457483376/999193433031835778/unknown.png",
+    title:"Agriculture Industry",
+  },
+  {
+    id:3,
+    img:"https://cdn.discordapp.com/attachments/992390118457483376/999193503068323940/unknown.png",
+    title:"Logistic Industry",
+  },
+  {
+    id:4,
+    img:"https://cdn.discordapp.com/attachments/992390118457483376/999193563877347358/unknown.png",
+    title:"Forestry Geological",
+  },
+  {
+    id:5,
+    img:"https://cdn.discordapp.com/attachments/992390118457483376/999193609964363776/unknown.png",
+    title:"Surveilliance Industry",
+  },
+ 
+  
+]
+
+
+  
+
