941e0c7e95e63fa645b321a564ea705eee20b672
mohitpanchal09 <90472961+mohitpanchal09@users.noreply.github.com>
2022-08-04 23:24:51 +0530

    Update NewSlider.jsx

--- src/components/NewSlider.jsx
+++ src/components/NewSlider.jsx
@@ -2,13 +2,14 @@ import AliceCarousel from 'react-alice-carousel';
 import "react-alice-carousel/lib/alice-carousel.css";
 import styled from 'styled-components';
 import React from 'react'
-import image1 from '../img1.png'
-import image2 from '../img2.png'
+// import image1 from '../img/img1.png'
+// import image2 from '../img/img2.png'
 import { mobile, mobile8 } from '../responsive';
 const Container = styled.div`
 display:flex;
 margin-left:auto;
 margin-right:auto;
+overflow:hidden;
 width: 50%;
   height: 500px;
   ${mobile({width:"80%"})}
@@ -17,7 +18,7 @@ width: 50%;
   
 `
 const Image = styled.img`
-
+overflow:hidden;
 width: 100%;
   height: 100%;
   object-fit: cover;
@@ -44,8 +45,8 @@ function NewSlider() {
         
         disableButtonsControls>
             
-        <Image src={image1}/>
-        <Image src={image2}/>
+        <Image src='https://cdn.discordapp.com/attachments/891025496601661504/1004808986110992415/Screenshot_1.png'/>
+        <Image src='https://cdn.discordapp.com/attachments/891025496601661504/1004809024761507940/Screenshot_3.png'/>
         
 
       </AliceCarousel>
