2de2bc203b897dcf52474fd58110cbab32ac4a96
mohitpanchal09 <90472961+mohitpanchal09@users.noreply.github.com>
2022-08-04 14:04:02 +0530

    Update Home2.jsx

--- src/pages/Home2.jsx
+++ src/pages/Home2.jsx
@@ -9,22 +9,31 @@ import Services from '../components/Services'
 import Tape1 from '../components/Tape1'
 import Tape2 from '../components/Tape2'
 import Industries from '../components/Industries'
-import Beginning from '../components/Beginning'
-
-import ExploreBox from '../components/ExploreBox'
-import ExploreBoxes from '../components/ExploreBoxes'
+// import Beginning from '../components/Beginning'
+import Mentors from '../components/Mentors'
+import NewSlider from '../components/NewSlider'
+// import ExploreBox from '../components/ExploreBox'
+// import ExploreBoxes from '../components/ExploreBoxes'
 import Products from '../components/Products'
+import AOS from "aos";
+import "aos/dist/aos.css"
+
 // import Projects from '../components/Projects/Projects'
 
 
 
 const Home2 = () => {
+
+  AOS.init({
+    duration:2000
+  })
   return (
     <div>
-      
+        
         <Navbar />
-        <Beginning />
-        <ExploreBox />
+        
+        
+        
         
        <Products />
         
@@ -32,6 +41,9 @@ const Home2 = () => {
         <Services />
         <Tape2 />
         <Industries />
+        <Mentors />
+        
+        <NewSlider />
         <Newsletter />
         <Footer />
     </div>
